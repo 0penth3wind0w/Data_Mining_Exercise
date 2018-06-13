628949ff3e7baa464386c53db47abb6c76f1edf3
0penth3wind0w <fc0029@yahoo.com.tw>
2018-06-13 22:10:44 +0800

    refactor hw1

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ The repository for Big Data Mining course.
 *Upload homework 2*  
 *Upload homework 3*  
 *Friday, 08. June 2018*  
-*Upload homework 5*  
+*Upload homework 5* 
+*Wednesday, 13. June 2018*
+*Refactor homework1* 
--- hw_1/homework1.py
+++ hw_1/homework1.py
@@ -1,37 +1,31 @@
+# This code is used with hadoop yarn
 import numpy as np
 from pyspark import SparkContext,  SparkConf
 from pyspark.mllib.stat import Statistics
 from math import sqrt
 
-data=sc.textFile("hdfs:///user/hadoop/data/household_power_consumption.txt").map(\
-	lambda x: x.split(";"))
-title = data.first()
-clean = data.filter(lambda x: x != title ).filter( lambda x: x[2] != "?" and x[3] != "?" and x[4] != "?"\ 	and x[5] != "?" ).map(lambda col: ( float(col[2]), float(col[3]), float(col[4]), float(col[5])))
+raw=sc.textFile("hdfs:///user/hadoop/data/household_power_consumption.txt").map(lambda x: x.split(";"))
+title = raw.first()
+data = raw.filter(lambda x: x != title ).filter( lambda x: x[2] != "?" and x[3] != "?" and x[4] != "?"and x[5] != "?" ).map(lambda col: ( float(col[2]), float(col[3]), float(col[4]), float(col[5])))
 
-summary = Statistics.colStats(clean)
+summary = Statistics.colStats(data)
 
-print(title[2], title[3], title[4], title[5])
+# 1 ==========
+print('[' + title[2] + ' ' + title[3] + ' ' + title[4] + ' ' + title[5] + ']')
 print(summary.min())
 print(summary.max())
-clean.filter( lambda x: x[0]).count()
-clean.filter( lambda x: x[1]).count()
-clean.filter( lambda x: x[2]).count()
-clean.filter( lambda x: x[3]).count()
 
-min_ga = summary.min()[0]
-min_gr = summary.min()[1]
-min_v = summary.min()[2]
-min_gi = summary.min()[3]
-
-range_ga = summary.max()[0] - min_ga
-range_gr = summary.max()[1] - min_gr
-range_v = summary.max()[2] - min_v
-range_gi = summary.max()[3] - min_gi
-
+# 2 ==========
 print(summary.mean())
-print(sqrt(summary.variance()[0]),sqrt(summary.variance()[1]),sqrt(summary.variance()[2]),\
-	sqrt(summary.variance()[3]),)
+print(sqrt(summary.variance()[0]),sqrt(summary.variance()[1]),sqrt(summary.variance()[2]),sqrt(summary.variance()[3]),)
 
-stds = clean.map(lambda col: [(col[0]-min_ga)/range_ga, (col[1]-min_gr)/range_gr,\
-	(col[2]-min_v)/range_v ,(col[3]-min_gi)/range_gi])
-stds.saveAsTextFile('/usr/lib/spark/data/result.txt')
+# 3 ==========
+def minMax_normalize(record, min_value, max_value):
+    return (record - min_value) / (max_value - min_value)
+
+mins = summary.min()
+maxs = summary.max()
+
+stds = data.map(lambda row: [minMax_normalize(row[0],mins[0],maxs[0]), minMax_normalize(row[1],mins[1],maxs[1]), minMax_normalize(row[2],mins[2],maxs[2]), minMax_normalize(row[3],mins[3],maxs[3])])
+# Save the result
+stds.saveAsTextFile('/result.txt')
